611cafcaff86f93b7eef9886653de59ff75da7ac
cotozelo <rafaelneto@gmail.com>
2018-04-18 00:24:56 -0300

    config head

--- src/main/java/br/com/nois/sa/rc/cors/CorsFilter.java
+++ src/main/java/br/com/nois/sa/rc/cors/CorsFilter.java
@@ -11,31 +11,38 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
-@Configuration
+//@Configuration
+@Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Vary", "Origin");
-
+		final HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
-		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				"Origin, X-Requested-With, Authorization, Content-Type, Accept, X-CSRF-TOKEN");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Expose-Headers", "Authorization");
+		response.setHeader("Access-Control-Request-Method", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
 
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
+		response.setHeader("Accept-Encoding", "gzip, deflate, br");
+		response.setHeader("Accept-Language", "en-US,en;q=0.9,pt;q=0.8");
+		response.setHeader("User-Agent",
+				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+		response.setHeader("Accept", "/");
+		response.setHeader("Connection", "keep-alive");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("x-frame-options", "ALLOW");
+
+		if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
 		} else {
 			chain.doFilter(req, res);
 		}
